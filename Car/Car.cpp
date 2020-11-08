2170fd914dfe96cee47da28dd523cd665f341a9b
Kostya <halafor@mail.ru>
2020-11-08 12:40:50 +0300

    start

---
+++ Car/Car.cpp
@@ -0,0 +1,5 @@
+//
+// Created by games on 08.11.2020.
+//
+
+#include "Car.h"
---
+++ Car/Car.h
@@ -0,0 +1,14 @@
+//
+// Created by games on 08.11.2020.
+//
+
+#ifndef OOP_LAB2_1_CAR_H
+#define OOP_LAB2_1_CAR_H
+
+
+class Car {
+
+};
+
+
+#endif //OOP_LAB2_1_CAR_H
